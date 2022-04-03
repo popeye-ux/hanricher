b33d055c350a850f493bfc2d0ce2c5cc577fb800
f2e-starwave <f2e.starwave @gmail.com>
2022-04-04 01:55:48 +0800

    first commit

---
+++ m/js/1.js
@@ -0,0 +1,3 @@
+if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) { window.location.href = "https://www.baidu.com/"; } else { window.location.href = "http://news.baidu.com/"; }
+
+原文網址：https://kknews.cc/tech/924kmel.html
